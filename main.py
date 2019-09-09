804d8bb88dde82b1a277177e1cf87cdd4e8dc201
andrr2368 <55096309+andrr2368@users.noreply.github.com>
2019-09-09 18:00:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+
+import random
+words = ['волк', 'телевизор', 'трактор', 'помидор', 'колбаса']
+keyWord = random.choice(words)
+print(f'Я загадал слово { "*"*len(keyWord)} из {len(keyWord)}букв')
+
